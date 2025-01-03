f3b8e9ad5a1f1a08e202028b6af556c8bd81a351
Christopher Tamayo <chriswtamayo@gmail.com>
2025-01-02 23:13:16 -1000

    Add contact logos and headshot

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,10 +1,13 @@
-import ContactsContainer from "./ContactsContainer"
+import ContactsContainer from "./ContactsContainer";
+import headshot from "../../assets/headshot.jpg";
 
 const Contact = () => {
   return (
     <div className="tile-container d-flex">
       <ContactsContainer />
-      <div className="headshot-tile">img</div>
+      <div className="headshot-tile">
+        <img src={headshot}></img>
+      </div>
     </div>
   )
 }
--- client/src/components/Contact/ContactsContainer.jsx
+++ client/src/components/Contact/ContactsContainer.jsx
@@ -1,22 +1,30 @@
 import "../Contact/Contact.css";
+import linkedIn from "../../assets/linkedin.png";
+import github from "../../assets/github.png";
+import email from "../../assets/email.png";
+import download from "../../assets/download.png";
 
 const ContactsContainer = () => {
   return (
     <div className="d-flex w-50">
       <div className="d-flex flex-column flex-grow-1">
-        <div className="contact-tile">
-          LinkedIn
+        <div className="d-flex flex-column justify-content-center align-items-center contact-tile linkedin-tile">
+          <img src={linkedIn} className="contact-logo"></img>
+          <p className="m-0">LinkedIn</p>
         </div>
-        <div className="contact-tile">
-          GitHub
+        <div className="d-flex flex-column justify-content-center align-items-center contact-tile github-tile">
+          <img src={github} className="contact-logo"></img>
+          <p className="m-0">GitHub</p>
         </div>
       </div>
       <div className="d-flex flex-column flex-grow-1">
-        <div className="contact-tile">
-          Email
+        <div className="d-flex flex-column justify-content-center align-items-center contact-tile email-tile">
+          <img src={email} className="contact-logo"></img>
+          <p className="m-0">Email</p>
         </div>
-        <div className="contact-tile">
-          Resume
+        <div className="d-flex flex-column justify-content-center align-items-center contact-tile resume-tile">
+          <img src={download} className="contact-logo"></img>
+          <p className="m-0">Resume</p>
         </div>
       </div>
     </div>
