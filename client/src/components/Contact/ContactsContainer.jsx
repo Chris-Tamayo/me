d2c274bb855a3800a959afa819c0def283778d45
Christopher Tamayo <chriswtamayo@gmail.com>
2025-01-04 20:42:32 -1000

    Fix contact tile square size

--- client/src/components/Contact/ContactsContainer.jsx
+++ client/src/components/Contact/ContactsContainer.jsx
@@ -10,13 +10,13 @@ const ContactsContainer = () => {
     <div className="d-flex w-50">
       <div className="d-flex flex-column flex-grow-1">
         <a href="https://www.linkedin.com/in/christopher-tamayo/" target="new">
-          <div className="d-flex flex-column justify-content-center align-items-center contact-tile linkedin-tile">
+          <div className="d-flex flex-column justify-content-center align-items-center contact-tile">
               <img src={linkedIn} className="contact-logo"></img>
               <p className="m-0">LinkedIn</p>
           </div>          
         </a>
         <a href="https://github.com/Chris-Tamayo" target="new">
-          <div className="d-flex flex-column justify-content-center align-items-center contact-tile github-tile">
+          <div className="d-flex flex-column justify-content-center align-items-center contact-tile">
             <img src={github} className="contact-logo"></img>
             <p className="m-0">GitHub</p>
           </div>
@@ -24,13 +24,13 @@ const ContactsContainer = () => {
       </div>
       <div className="d-flex flex-column flex-grow-1">
         <a href="mailto:chriswtamayo@gmail.com">
-          <div className="d-flex flex-column justify-content-center align-items-center contact-tile email-tile">
+          <div className="d-flex flex-column justify-content-center align-items-center contact-tile">
             <img src={email} className="contact-logo"></img>
             <p className="m-0">Email</p>
           </div>
         </a>
         <a href={resume} download>
-          <div className="d-flex flex-column justify-content-center align-items-center contact-tile resume-tile">
+          <div className="d-flex flex-column justify-content-center align-items-center contact-tile">
             <img src={download} className="contact-logo"></img>
             <p className="m-0">Resume</p>
           </div>
