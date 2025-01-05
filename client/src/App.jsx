2650a69512c3aba58627cdc3a606cce3cb8f2aee
Christopher Tamayo <chriswtamayo@gmail.com>
2025-01-04 20:42:59 -1000

    Add initial view projects button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,15 @@ import './App.css'
 import Contact from './components/Contact/Contact'
 import Education from './components/Education/Education'
 import Experiences from './components/Experience/Experiences'
+import FeaturedProject from './components/FeaturedProject/FeaturedProject'
 import Intro from './components/Intro/Intro'
 import Skills from './components/Skills/Skills'
+import ViewProjectsButton from './components/ViewProjectsButton/ViewProjectsButton'
 
 function App() {
   return (
     <div className="d-flex justify-content-center align-items-center w-100 h-100">
-      <div className="d-flex flex-row">
+      <div className="d-flex flex-row flex-wrap">
         <div className="d-flex flex-column">
           <Intro />
           <Contact />
@@ -19,6 +21,8 @@ function App() {
         </div>
         <div className="d-flex flex-column">
           <Education />
+          <FeaturedProject />
+          <ViewProjectsButton />
         </div>
       </div>
     </div>
---
+++ client/src/components/ViewProjectsButton/ViewProjectsButton.jsx
@@ -0,0 +1,14 @@
+import "../ViewProjectsButton/ViewProjectsButton.css";
+
+const ViewProjectsButton = () => {
+  return (
+    <div className="view-projects-button flex-grow-1 d-flex align-items-center">
+      <div className="d-flex flex-row justify-content-between align-items-center w-100 mx-3">
+        <p className="m-0"><strong>View all projects</strong></p>
+        <span class="material-symbols-outlined">north_east</span>
+      </div>
+    </div>
+  )
+}
+
+export default ViewProjectsButton
