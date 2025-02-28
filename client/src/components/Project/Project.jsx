d18181f56179e5d98af255aef9c822e9af13f15f
Christopher Tamayo <chriswtamayo@gmail.com>
2025-02-28 00:17:20 -0800

    Modify project spacing

--- client/src/components/Project/Project.jsx
+++ client/src/components/Project/Project.jsx
@@ -4,9 +4,9 @@ const Project = ({ name, description, image, skills }) => {
   return (
     <div className="project-container tile align-self-stretch">
       <div className="d-flex flex-column">
-        <img src={image} className="project-img mb-1"></img>
+        <img src={image} className="project-img mb-3"></img>
         <strong className="mb-1">{name}</strong>
-        <p className="project-desc mb-1">{description}</p>
+        <p className="project-desc mb-2">{description}</p>
         <div className="d-flex flex-wrap skills-container">
           {skills.map((skill, index) => (
             <span className="skill">{skill}</span>
