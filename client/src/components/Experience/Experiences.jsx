455cc9e270ecff0d9a4ba32fa8cd10fa1a55a656
Christopher Tamayo <chriswtamayo@gmail.com>
2025-03-19 22:57:28 -0700

    Fix dates

--- client/src/components/Experience/Experiences.jsx
+++ client/src/components/Experience/Experiences.jsx
@@ -9,8 +9,8 @@ const Experiences = () => {
       <strong>Experience</strong>
       <div className="d-flex flex-column pb-2">
         <Experience company="Amazon" role="Software Development Engineer" dates="Sep. 2024 - Present" logo={amazon}/>
-        <Experience company="Amazon" role="Software Development Engineer Intern" dates="June 2023 - Sep. 2024" logo={amazon}/>
-        <Experience company="Amazon" role="Software Development Engineer Intern" dates="June 2022 - Sep. 2023" logo={amazon}/>
+        <Experience company="Amazon" role="Software Development Engineer Intern" dates="June 2023 - Sep. 2023" logo={amazon}/>
+        <Experience company="Amazon" role="Software Development Engineer Intern" dates="June 2022 - Sep. 2022" logo={amazon}/>
         <Experience company="NoLi Fitness" role="App Development Intern" dates="Nov. 2021 - Mar. 2022" logo={noli}/>
         <Experience company="American Savings Bank" role="IT Banking Systems Intern" dates="June 2021 - Aug. 2021 " logo={asb}/>
       </div>
