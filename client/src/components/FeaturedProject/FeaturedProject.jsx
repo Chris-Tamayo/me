c620c869b2b0e039d6a09f4847eb3228430b6016
Christopher Tamayo <chriswtamayo@gmail.com>
2025-03-19 23:07:25 -0700

    Add link to featured project

--- client/src/components/FeaturedProject/FeaturedProject.jsx
+++ client/src/components/FeaturedProject/FeaturedProject.jsx
@@ -3,11 +3,11 @@ import pt from "../../assets/pt.png";
 
 const FeaturedProject = () => {
   return (
-    <div className="tile">
+    <div className="tile featured-project">
       <strong>Featured Project</strong>
       <div className="d-flex flex-column">
         <img src={pt} className="project-img"></img>
-        <p>Potenti</p>
+        <a href="https://github.com/liamahearn/PT-Wearable" className="featured-project-name" target="new"><strong className="d-flex align-items-center">Potenti<span class="material-symbols-outlined mx-1">open_in_new</span></strong></a>
         <p className="project-desc">PT recovery website w/ motion tracking wearables</p>
       </div>
     </div>
